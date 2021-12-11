5042f9f810c55a208e298075d25297fa860837df
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-12-11 12:33:34 +0630

    e-commerce api porject setup

---
+++ e-commerce/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+// express
+const express = require('express');
+const app = express();
+
+// database
+const connectDB = require('./db/connect');
+
+const PORT = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server is running on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ e-commerce/middleware/not-found.js
@@ -0,0 +1,4 @@
+const notFound = (req, res) =>
+  res.status(404).send(`Not found - ${req.originalUrl}`);
+
+module.exports = notFound;
