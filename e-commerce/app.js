3e1d5674f531af39e41e3634e18dafeb4bb65f58
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-12-11 12:36:54 +0630

    config mongodb

--- e-commerce/app.js
+++ e-commerce/app.js
@@ -10,7 +10,9 @@ const connectDB = require('./db/connect');
 const PORT = process.env.PORT || 5000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    const conn = await connectDB(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
     app.listen(PORT, console.log(`Server is running on port ${PORT}...`));
   } catch (error) {
     console.log(error);
